6ef48ba98068c05d845ee19136b425da62dece33
carlos gonzalez <cgonzalez990811@gmail.com>
2024-02-13 15:34:15 -0500

    productos y categorias añadidos

--- front-coffe/src/context/QuioscoProvider.jsx
+++ front-coffe/src/context/QuioscoProvider.jsx
@@ -1,14 +1,16 @@
 import { Children, createContext, useEffect, useState } from "react"
-import { categorias as categoriasDB } from "../data/categorias";
 import { toast } from "react-toastify";
+import clienteAxios from "../config/axios";
+
+
 
 const QuioscoContext = createContext();
 
 
 const QuioscoProvider = ({children}) => {
 
-    const [categorias, setCategorias] = useState(categoriasDB);
-    const [categoriaActual, setCategoriaActual] = useState(categorias[0]);
+    const [categorias, setCategorias] = useState([]);
+    const [categoriaActual, setCategoriaActual] = useState({});
     const [modal, setModal] = useState(false);
     const [producto, setProducto] = useState({});
     const [pedido, setPedido] = useState([]);
@@ -19,6 +21,20 @@ const QuioscoProvider = ({children}) => {
         const nuevoTotal = pedido.reduce((total, producto)=>(producto.precio * producto.cantidad) + total, 0)
         setTotal(nuevoTotal);
     },[pedido])
+
+    const obtenerCategorias = async () => {
+        try {
+            const {data} = await clienteAxios('/api/categorias');
+            setCategorias(data.data);
+            setCategoriaActual(data.data[0])
+        } catch (error) {
+            console.log(error);            
+        }
+    }
+
+    useEffect(()=>{
+        obtenerCategorias();
+    },[]);
 
     const handleClickCategoria = id => {
         const categoria = categorias.filter(categoria => categoria.id === id)[0]
